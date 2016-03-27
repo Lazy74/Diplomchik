fdbe433f092463399b229d33b96fb37fc9ef115a
Kasyanenko.ID <lazy-777@yandex.ru>
2016-03-27 21:00:45 +0500

    модель игрока

---
+++ Dplm/Models/People.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dplm
+{
+    public class People
+    {
+        public string UserLogin { get; set; }
+
+        public string UserPass { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string FamiluName { get; set; }
+
+        public string Name { get; set; }
+
+        public string Birthday { get; set; }
+
+        public string LinkVK { get; set; }
+    }
+}
