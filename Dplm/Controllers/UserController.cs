fcdf73933a28c547d3ee2fc6ff714fb7d4c68105
Kasyanenko.ID <lazy-777@yandex.ru>
2016-03-28 01:22:04 +0500

    Сервер принимает логин и пароль

---
+++ Dplm/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Web;
+using System.Web.Http;
+using System.Web.Services.Description;
+using Newtonsoft.Json.Linq;
+
+namespace Dplm.Controllers
+{
+    public class UserController : ApiController
+    {
+        // GET: User
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public void AuthorizeUser(string login, string pass)
+        {
+            if (login == pass)
+            {
+                return;
+            }
+        }
+    }
+}
--- Dplm/Scripts/StartPage.js
+++ Dplm/Scripts/StartPage.js
@@ -1,4 +1,11 @@
-﻿var viewModel = new ViewModel();
+﻿var model = {
+    authorizeUser: function (login, pass) {
+        console.log("AuthorizeUser");
+        $.get("/api/User/AuthorizeUser/", {Login: login, Pass: pass});
+    }
+}
+
+var viewModel = new ViewModel();
 
 function loadPage() {
     ko.applyBindings(viewModel);
@@ -10,6 +17,7 @@ function ViewModel() {
     this.userPass = ko.observable();
 
     this.authorizeUser = function () {
+        model.authorizeUser(this.userLogin(), this.userPass());
     }
 
     this.createUser = function () {
