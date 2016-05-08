17726e22dd082e4760d71838722bb0c3ec9145f5
KasyanenkoID <lazy-777@yandex.ru>
2016-05-08 18:50:56 +0600

    Изменения на страницы игры для отправки ответов на сервер + JS

---
+++ Dplm/Scripts/GameplayPage.js
@@ -0,0 +1,32 @@
+﻿var model = {
+    Answer: function (answer) {
+        console.log("toAnswer");
+        //debugger;
+        $.get("/GamePlay/AnswerСheck/", {
+            answer: answer
+        })
+            .done(function (r) {
+                debugger;
+                alert("Верный");
+                //location.pathname = "";    // строка пути (относительно хоста)
+            })
+            .fail(function (r) {
+                debugger;
+                alert("Ответ \"" + answer + "\" не верный");
+            });
+    }
+}
+
+var viewModel = new ViewModel();
+
+function loadPage() {
+    ko.applyBindings(viewModel);
+}
+
+function ViewModel() {
+    this.playerAnswer = ko.observable();
+
+    this.toAnswer = function () {
+        model.Answer(this.playerAnswer());
+    }
+}
