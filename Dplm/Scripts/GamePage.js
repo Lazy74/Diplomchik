87530576e19476c67c2bbb3a975b81a79b323cd3
KasyanenkoID <lazy-777@yandex.ru>
2016-05-10 22:32:09 +0600

    Со страницы игры можно зайти в САМУ игру и поиграть =)

---
+++ Dplm/Scripts/GamePage.js
@@ -0,0 +1,20 @@
+﻿var model = {
+    EnterTheGame: function () {
+        console.log("2");
+        location.pathname = "Gameplay/" + window.currentGameId;
+    }
+}
+
+var viewModel = new ViewModel();
+
+function loadPage() {
+    ko.applyBindings(viewModel);
+}
+
+function ViewModel() {
+
+    this.enterTheGame = function () {
+        model.EnterTheGame();
+        console.log("1");
+    }
+}
