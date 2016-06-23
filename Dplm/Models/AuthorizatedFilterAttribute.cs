9efdd4e9c472d39c64e17e45ad7f46fd5136be64
Kasyanenko.ID <lazy-777@yandex.ru>
2016-06-23 15:02:25 +0500

    Проверка на авторизацию в ActionFilter

---
+++ Dplm/Models/AuthorizatedFilterAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dplm.Models
+{
+    public class AuthorizatedFilterAttribute : FilterAttribute, IActionFilter
+    {
+        //public override void OnActionExecuting(ActionExecutingContext filterContext)
+        //{
+        //    HttpCookie cookie = filterContext.RequestContext.HttpContext.Request.Cookies["hash"];
+
+        //    filterContext.RequestContext.HttpContext.Response.SetCookie(MyCookies.UpdateCookieSession(cookie));
+        //}
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Проверка на авторизацию
+            //filterContext.Result = new HttpUnauthorizedResult();
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            
+        }
+    }
+}
